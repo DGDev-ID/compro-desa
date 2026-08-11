ae13899437cbaaaaf01d1bcf83c3f3269d947fd2
Elang Argawana <elangargawana.15@gmail.com>
2026-08-11 18:50:59 +0700

    iya ara

--- src/services/banner.service.js
+++ src/services/banner.service.js
@@ -4,7 +4,7 @@ import api from '@/api/axios'
  * Fetch active banners from backend.
  */
 export async function getBanners() {
-  const res = await api.get('/banners')
+  const res = await api.get('/banner')
   return (res.data.data || []).map((b) => ({
     image: b.background_image_url,
     caption: b.title || b.judul || '',
